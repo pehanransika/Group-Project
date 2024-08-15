7befbc5c57c4869238224cfb1526309ef97f5604
Naleeka Kumarasinghe <knaleeka845@gmail.com>
2024-08-15 22:13:36 +0530

    added a small notification at the bottom when post submission clicked

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,11 @@ const addPostBtn = document.querySelector(".addPost-btn")
 const body = document.querySelector("body");
 const popupCloseBtn = document.getElementById("popup-close-btn")
 const popupClearBtn = document.getElementById("popup-clear-btn")
+const popuppostBtn = document.getElementById("popup-post-btn")
 const navRadios = document.querySelectorAll('input[name="nav"]');
+const notificationMsg = document.querySelector(".notification-msg");
+const postLikeBtns = document.querySelectorAll(".post .like");
+let notificationMsgContent = "Your vote has been successfully added !";
 
 sideMenuBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -51,3 +55,23 @@ navRadios.forEach(radio => {
         }
     });
 });
+
+popuppostBtn.addEventListener("click", () => {
+    // implement validation here
+    let validity = true
+    if (validity) {
+        notificationMsgContent = "Your post has been successfully added !"
+        notificationMsg.innerHTML = notificationMsgContent;
+        body.classList.remove("overlay-active")
+        body.classList.add("noti-active")
+        setTimeout(() => {
+            body.classList.remove("noti-active")
+        }, 3000)
+    }
+})
+
+postLikeBtns.forEach(likeBtn => {
+    likeBtn.addEventListener("click", () => {
+        likeBtn.closest(".post").classList.toggle("reacted")
+    })
+})
